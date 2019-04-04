9b88a8a68161c730e60f3d64d601a902858c18de
qanastek <yanis.labrak@alumni.univ-avignon.fr>
2019-04-04 15:40:36 +0200

    change the way the sidebar be active - add JQuery

--- js/EasySchool.js
+++ js/EasySchool.js
@@ -1,5 +1,11 @@
 jQuery(document).ready(function($)
 {
+	/*Full URL*/
 	var URL = window.location.protocol + "//" + window.location.host + window.location.pathname + window.location.search
+	
+	/*Navbar*/
 	$('.nav-item > a[href="'+URL+'"]').parent().addClass('active');
+
+	/*Sidebar for "présentation"*/
+	$('.item_menu[href="'+URL+'"]').addClass('item_active_menu');
 })
--- sidebar_presentation.php
+++ sidebar_presentation.php
@@ -1,100 +1,38 @@
 <!-- Side Menu -->
 	<div class="col-xs-12 col-sm-12 col-md-3 col-xl-3 mobile_convertion" style="text-align: left; padding-right: 2%;">
 
-		<?php $slug = basename(get_permalink()); ?>
-
 		<p>
-			<a href="<?php echo get_permalink( get_page_by_title( "PRÉSENTATION DE L'ÉTABLISSEMENT" ) ); ?>" class="item_menu
-				<?php
-
-					if ($slug == "presentation-de-letablissement") {
-
-						echo("item_active_menu");
-
-					}
-
-				?>
-			">
+			<a href="<?php echo get_permalink( get_page_by_title( "PRÉSENTATION DE L'ÉTABLISSEMENT" ) ); ?>" class="item_menu">
 				Présentation de l'école
 			</a>
 		</p>
 
 		<p class="top_bar_menu">
-			<a href="<?php echo get_permalink( get_page_by_title( "L’équipe éducative" ) ); ?>" class="item_menu
-				<?php
-
-					if ($slug == "lequipe-educative") {
-
-						echo("item_active_menu");
-
-					}
-
-				?>
-			">
+			<a href="<?php echo get_permalink( get_page_by_title( "L’équipe éducative" ) ); ?>" class="item_menu">
 				L’équipe éducative
 			</a>
 		</p>
 
 		<p class="top_bar_menu">
-			<a href="<?php echo get_permalink( get_page_by_title( "Restauration scolaire" ) ); ?>" class="item_menu
-				<?php
-
-					if ($slug == "restauration-scolaire") {
-
-						echo("item_active_menu");
-
-					}
-
-				?>
-			">
+			<a href="<?php echo get_permalink( get_page_by_title( "Restauration scolaire" ) ); ?>" class="item_menu">
 				Restauration scolaire
 			</a>
 		</p>
 
 		<p class="top_bar_menu">
-			<a href="<?php echo get_permalink( get_page_by_title( "Activités péri-scolaires" ) ); ?>" class="item_menu
-				<?php
-
-					if ($slug == "activites-peri-scolaires") {
-
-						echo("item_active_menu");
-
-					}
-
-				?>
-			">
+			<a href="<?php echo get_permalink( get_page_by_title( "Activités péri-scolaires" ) ); ?>" class="item_menu">
 				Activités péri-scolaires
 			</a>
 		</p>
 
 		<p class="top_bar_menu">
-			<a href="<?php echo get_permalink( get_page_by_title( "Activités extra-scolaire" ) ); ?>" class="item_menu
-				<?php
-
-					if ($slug == "activites-extra-scolaire") {
-
-						echo("item_active_menu");
-
-					}
-
-				?>
-			">
+			<a href="<?php echo get_permalink( get_page_by_title( "Activités extra-scolaire" ) ); ?>" class="item_menu">
 				Activités extra-scolaire
 			</a>
 		</p>
 
 		<p class="top_bar_menu">
-			<a href="<?php echo get_permalink( get_page_by_title( "Liste des fournitures" ) ); ?>" class="item_menu
-				<?php
-
-					if ($slug == "liste-des-fournitures") {
-
-						echo("item_active_menu");
-
-					}
-
-				?>
-			">
+			<a href="<?php echo get_permalink( get_page_by_title( "Liste des fournitures" ) ); ?>" class="item_menu">
 				Liste des fournitures
 			</a>
 		</p>
